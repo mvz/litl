7be32e0d2b92208c5b8f59abe448543100cfae1f
Matijs van Zuijlen <matijs@matijs.net>
2013-09-20 08:44:57 +0200

    Add integration test for simple delimited expression

--- test/litl/template_test.rb
+++ test/litl/template_test.rb
@@ -13,4 +13,15 @@ describe Litl::Template do
 
     rendered_result(src).must_equal expected
   end
+
+  it "handles a simple HTML example with an ivar" do
+    src = "(html (h1 Foo) (p (= @foo .) bar))"
+
+    @foo = 'contents-of-foo'
+    p self
+
+    expected = "<html><h1>Foo</h1><p>contents-of-foo bar</p></html>"
+
+    rendered_result(src).must_equal expected
+  end
 end
